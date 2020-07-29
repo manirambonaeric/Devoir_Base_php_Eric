a8929c7d52c841a508d804d1201dccfdad50504e
MANIRAMBONA Eric <maniramboanae@gmail.com>
2020-07-29 09:32:15 +0200

    declaration des variables

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+// les variables
+$t=2;
+$p=4;
+function somme($t,$p)
+{
+    $a=$t;
+    $b=$p;
+    $r=$t+$p;
+    echo '<h2>'.'1)La somme de a et b est: ' .$r . '</h2>';
+}
+somme(2,4);
 
-
-
-
-
+// Pour les booleens 
 
 
 
